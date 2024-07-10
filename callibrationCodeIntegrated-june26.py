f996b0a55683a272f2dcb690c18180b814252f5f
zippy-zip <122580849+zippy-zip@users.noreply.github.com>
2024-07-10 13:37:35 -0700

    Version 4

--- callibrationCodeIntegrated-june26.py
+++ callibrationCodeIntegrated-june26.py
@@ -9,7 +9,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-## File Library
+''' file library '''
     # Old Callibration (load cell attached at one screw) (WEEK 1)
     # FORWARD TT "C:\Users\zoech\Desktop\Jacks Reserach 2024\Code\jun26-forwardFunctionalDataSet.txt"
     
@@ -21,7 +21,7 @@ import numpy as np
     # REVERSE TT "C:\Users\zoech\Desktop\Jacks Reserach 2024\Code\Jul1 Calibration two screws in L bracket negative cal factor.txt"
     
     
-### CREATING FILE OF RAW DATA
+''' reading input file '''
 
 # Input file path
 input_file_path =  r"C:\Users\zoech\Desktop\Jacks Reserach 2024\Code\Callibration Code\Jul1 Calibration two screws in L bracket positive cal factor.txt"
@@ -47,28 +47,29 @@ for i in range(len(readings)):
     else:
         ratio = float(readings[i]/weights[i])
     ratios.append(round(ratio, 5))
- 
- 
-# Write data to the output file
-# with open(output_file_path, 'x') as outfile:
-#     outfile.write("Reading\t     Weight (g)\t     Ratios\n")
+
+
+''' writing to an output file '''
+
+with open(output_file_path, 'x') as outfile:
+    outfile.write("Reading\t     Weight (g)\t     Ratios\n")
    
-#     # Set number of rows
-#     max_rows = max(len(readings), len(weights),len(ratios))
+    # Set number of rows
+    max_rows = max(len(readings), len(weights),len(ratios))
    
-#     for i in range(max_rows):
-#         reading = readings[i] if i < len(readings) else ''
-#         weight = weights[i] if i < len(weights) else ''
-#         ratio = ratios[i] if i < len(ratios) else ''
-#         outfile.write(f"{reading}\t     {weight}\t     {ratio}\n"   )
+    for i in range(max_rows):
+        reading = readings[i] if i < len(readings) else ''
+        weight = weights[i] if i < len(weights) else ''
+        ratio = ratios[i] if i < len(ratios) else ''
+        outfile.write(f"{reading}\t     {weight}\t     {ratio}\n"   )
     
-#     avg_ratio = sum(ratios)/(len(ratios)-1)
-#     outfile.write(f"\nAverage Ratio: {avg_ratio:.5f}\n")
+    avg_ratio = sum(ratios)/(len(ratios)-1)
+    outfile.write(f"\nAverage Ratio: {avg_ratio:.5f}\n")
  
-# print(f"Data has been written to {output_file_path}")
+print(f"Data has been written to {output_file_path}")
  
 
-### CREATING GRAPH WITH INPUT DATA
+''' plotting '''
 
 # Known Weight
 x = weights
